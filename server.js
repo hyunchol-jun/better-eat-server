5f2e0abffc9141d97b7494a8fbe43039d0e1110f
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-15 14:46:59 -0400

    Create server.js and add scripts to package.json

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+// const BASE_URL = process.env.BASE_URL;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(PORT, () => {
+    console.log("Server running on PORT: ", PORT);
+})
