749441ad71f20f717628b3624c36224ab6e1b3f0
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-17 19:42:11 -0400

    Use env variables for database info

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,15 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+require("dotenv").config();
+
 module.exports = {
     client: 'mysql2',
     connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "rootroot",
-      database: "bettereat",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       charset: "utf8"
     }
 };
