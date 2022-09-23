48970b0c97d15c6533d7ae814d51b58dafa1b358
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-09-23 14:58:55 -0400

    Add recipe seed data

---
+++ seed_data/recipes.js
@@ -0,0 +1,77 @@
+module.exports = [
+    {
+        "ingredients": [
+            {
+                "id": 11206,
+                "name": "cucumbers",
+                "original": "2 organic cucumbers, sliced",
+            },
+            {
+                "id": 2047,
+                "name": "salt",
+                "original": "3 tablespoons of salt",
+            },
+            {
+                "id": 11124,
+                "name": "carrot",
+                "original": "1 medium organic carrot, thinly sliced",
+            },
+            {
+                "id": 11291,
+                "name": "scallions",
+                "original": "3 organic scallions, sliced, include green and white parts",
+            },
+            {
+                "id": 2031,
+                "name": "red pepper powder",
+                "original": "cup of red pepper powder (can find at Korean market), plus more to taste",
+            },
+            {
+                "id": 11215,
+                "name": "garlic",
+                "original": "2 cloves organic garlic, minced",
+            },
+            {
+                "id": 11216,
+                "name": "ginger",
+                "original": "1/2 inch of grated organic ginger",
+            },
+            {
+                "id": 6179,
+                "name": "fish sauce",
+                "original": "1 tablespoon of fish sauce (can find at grocery store in Asian section)",
+            },
+            {
+                "id": 9152,
+                "name": "juice of lemon",
+                "original": "1/2 Juice of a lemon",
+            },
+            {
+                "id": 19296,
+                "name": "honey",
+                "original": "1 tablespoon of honey",
+            },
+            {
+                "id": 4058,
+                "name": "sesame oil",
+                "original": "1 teaspoon of toasted sesame oil",
+            }
+        ],
+        "title": "Zomppa's Kimchi",
+        "api_id": 665680,
+        "ready_min": 45,
+        "image": "https://spoonacular.com/recipeImages/665680-556x370.jpg",
+        "cuisines": [
+            "Korean",
+            "Asian"
+        ],
+        "diets": [
+            "gluten free",
+            "dairy free",
+            "paleolithic",
+            "primal",
+            "pescatarian"
+        ],
+        "instructions": "Peel skin off of cucumbers.\nSlice the cucumbers in  of inch slices.\nPlace cumbers in a bowl with salt.  Let sit for 15 minutes.\nWith hands, squeeze liquid out of the cucumbers.\nPlace cucumbers back in bowl.  Add the garlic, ginger, red pepper powder, fish sauce, lemon juice, sesame oil and honey.\nWith hands, mush all ingredients together.  Taste.\nAdd more hot pepper, fish sauce or honey to adjust the taste to your liking.  8. Place in airtight container and let refrigerate for 3 hours before enjoying.",
+    }
+];
